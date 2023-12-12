f4432f73219ec3112ed910f60dfc6a023bef650d
Achmad Khosyi' A. R <ramandanta@gmail.com>
2023-12-13 01:35:27 +0700

    hehe

--- pendaftaran_siswa.sql
+++ pendaftaran_siswa.sql
@@ -15,12 +15,10 @@ CREATE TABLE `calon_siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `calon_siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`) VALUES
-(1, 'Akmal Sulthon', 'Asrama Mahasiswa ITS Blok E', 'Laki-laki', 'Islam', 'SMAN 1 Ponorogo'),
-(2, 'Duevano Fairuz', 'Perumahan GSI, Kedurus', 'Laki-laki', 'Islam', 'SMAN 6 Surabaya'),
-(3, 'Ken Anargya', 'Perumdos ITS Blok U', 'Laki-laki', 'Islam', 'SMAN 7 Depok'),
-(4, "Achmad Khosyi'", 'Kejawan Putih Tambak', 'Laki-laki', 'Islam', 'SMAN 1 Gresik'),
-(5, 'Nadya Zuhria', 'Gang Mawar, Gebang Putih', 'Perempuan', 'Islam', 'MAN 2 Ponorogo'),
-(6, 'Abdullah Yasykur', 'Pondok Pesantren Darussalam, Keputih', 'Laki-laki', 'Islam', 'Pondok Pesantren Tebuireng Jombang');
+(1, 'Kevin Hierarchy', 'Tambak Pinggir Kali, Gubeng', 'Male', 'Christian', 'SMPK Haung To'),
+(2, 'Nakashima Haruka', 'GunungSari, Manyar', 'Female', 'Hindu', 'SMP 3 Manyar'),
+(3, 'Akmal Ahyun', 'Pondok Kejawan Hijau', 'Male', 'Islam', 'SMAN 1 Surabaya'),
+(4, 'Xie Xidaidai', 'Kampung Cina, Surabaya', 'Female', 'Confucian', 'SMAN 25 Surabaya');
 
 ALTER TABLE `calon_siswa`
   ADD PRIMARY KEY (`id`);
